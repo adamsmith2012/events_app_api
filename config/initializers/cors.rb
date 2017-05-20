5f5d931874f0a0ff06aa8a52984edc37de6aec7e
588870 <adamsmith@c1mrq060hf4h.attlocal.net>
2017-05-20 10:00:28 -0700

    Changed allowed origins

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'events-app-api.herokuapp.com', 'localhost:8000'
+    origins 'https://sports-events.herokuapp.com', 'localhost:8000'
 
     resource '*',
       headers: :any,

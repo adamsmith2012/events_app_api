3985cd0e54d86c11222b4fac5ab6850f2da9c259
Ciampije <josephciampi@C1MRQ06AHF4H.fios-router.home>
2017-05-22 12:35:53 -0400

    updated icons

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,12 +7,12 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 Sport.create ([
-		{ name: 'Basketball', img: 'https://lh3.googleusercontent.com/Eg4qDeTD4Efy07T52jJj5DUG4oRyaetRH0UGfkHoT0jBeFlCoVdAVtLNuMq2vR1XkUbv=w300'},
-		{ name: 'Soccer', img: 'https://lh3.googleusercontent.com/sf4KjBUdqqivaX0sIWrgnemtENCIbsZ4s0POLeLxO4kJnmzhjlat0EIG6nXrLB8J1e8=w300'},
-		{ name: 'Football', img: 'http://images.clipartpanda.com/clipart-football-1293842064_Clipart_Free.png'},
-		{ name: 'Softball', img: 'https://www.staffordschools.net/cms/lib/VA01818723/Centricity/Domain/4008/softball.png'},
-		{ name: 'Baseball', img: 'http://www.longmeadowbaseball.org/wp/wp-content/uploads/2016/02/lbsa-504-baseball-1-300x300.png'},
-		{ name: 'Kickball', img: 'https://lh3.googleusercontent.com/enB45I9fPdhld_YutIyU84bwjuxh6yTEhEfsdMcoxFpDdxKam4rAg_NUPIVtLY4d-Xk=w300'},
+		{ name: 'Basketball', img: 'https://image.flaticon.com/icons/svg/164/164992.svg'},
+		{ name: 'Soccer', img: 'https://image.flaticon.com/icons/svg/140/140389.svg'},
+		{ name: 'Football', img: 'https://image.flaticon.com/icons/svg/164/164993.svg'},
+		{ name: 'Softball', img: 'https://image.flaticon.com/icons/svg/140/140411.svg'},
+		{ name: 'Baseball', img: 'https://image.flaticon.com/icons/svg/164/164991.svg'},
+		{ name: 'Kickball', img: 'https://image.flaticon.com/icons/svg/421/421170.svg'},
 ])
 
 
